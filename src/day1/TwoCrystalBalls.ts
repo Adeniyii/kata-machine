7758eb3bbb873c5482f7a18ce707ddc5005f52fe
Adeniyii <ifedayoadeniyi@gmail.com>
2023-09-02 20:37:15 +0100

    feat: implement two crystal ball problem
    first try babyyyyyyy

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,2 +1,14 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+  const sqrtjump = Math.floor(Math.sqrt(breaks.length))
+  for (let i = 0; i < breaks.length; i = sqrtjump + i) { // jump by sqrt(n) intervals till a breaking point is reached
+    if (breaks[i] === true) {
+      if (i === 0) return 0; // if ball breaks at first index, return 0
+      for (let j = i - sqrtjump; j < i; ++j) { // set j to last unbroken jump point (i - sqrtjump); then traverse linearly till exact breaking point is found
+        if (breaks[j] === true) {
+          return j;
+        }
+      }
+    }
+  }
+  return -1;
 }
