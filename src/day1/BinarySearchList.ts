496773a3711e6030b439e05a584656072a6739ae
Adeniyii <ifedayoadeniyi@gmail.com>
2023-09-02 20:36:29 +0100

    feat: implement binary seaarch
    also first try ;)

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,17 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
 
+  if (!haystack || !haystack.length) return false;
+  if (haystack.length === 1) {
+    return haystack[0] === needle;
+  }
+
+  let m = Math.floor(haystack.length / 2)
+
+  if (haystack[m] === needle) {
+    return true;
+  } else if (haystack[m] > needle) {
+    return bs_list(haystack.slice(0, m), needle)
+  } else {
+    return bs_list(haystack.slice(m + 1, haystack.length), needle)
+  }
 }
