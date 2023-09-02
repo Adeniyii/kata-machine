b22d27c3c2c6b4bc5b9dea07012e76a3cc9a826b
Adeniyii <ifedayoadeniyi@gmail.com>
2023-09-02 22:33:52 +0100

    feat: implement bubble sort
    first try again.. this is getting embarassing

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,17 @@
 export default function bubble_sort(arr: number[]): void {
 
+  let swapped = true;
+  while (swapped) {
+    let iter = 0;
+    swapped = false;
+    for (let i = 0; i < arr.length - iter; ++i) {
+      if (arr[i] > arr[i + 1]) {
+        let tmp = arr[i]
+        arr[i] = arr[i + 1]
+        arr[i + 1] = tmp;
+        swapped = true;
+        iter++;
+      }
+    }
+  }
 }
