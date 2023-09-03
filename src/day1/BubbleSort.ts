fc4ad221d639436a7960776af83fa88007cd7f82
Adeniyii <ifedayoadeniyi@gmail.com>
2023-09-03 10:01:55 +0100

    feat: comparison to prime's implementation

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -15,3 +15,9 @@ export default function bubble_sort(arr: number[]): void {
     }
   }
 }
+
+// this implementation has a much better best case time complexity than
+// prime's implementation. the reason is that i only attempt to traverse 
+// the array IF there was a swap in the previous step. The implication of this 
+// is that, if the array is already sorted, the practial time complexity will not 
+// be O(n^2) rather it will be O(n): we traverse the array fully one time.
