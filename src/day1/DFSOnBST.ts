fde49c7681dab45aaea42ab2510fb801815922b9
Adeniyii <ifedayoadeniyi@gmail.com>
2023-09-05 23:00:12 +0100

    feat: implement binary search tree
    easy peasy

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,16 @@
+
+function walk(head: BinaryNode<number> | null, needle: number): boolean {
+  if (!head) return false;
+  if (needle === head.value) return true;
+
+  if (needle > head.value) {
+    return walk(head.right, needle)
+  } else {
+    return walk(head.left, needle)
+  }
+}
+
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
 
+  return walk(head, needle)
 }
