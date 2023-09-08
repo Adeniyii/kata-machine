0b0b4114f7de16452e73345dd5308a7c9d0f847c
Adeniyii <ifedayoadeniyi@gmail.com>
2023-09-08 15:07:08 +0100

    feat: implement graph path finding using adjacency list and dfs
    first try.. not counting compiler errors ;)

--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,31 @@
 export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
 
+  let seen = new Array(graph.length).fill(false)
+  let path: number[] = []
+  walk(graph, source, needle, seen, path)
+  return path.length ? path : null
+}
+
+function walk(graph: WeightedAdjacencyList, source: number, needle: number, seen: boolean[], path: number[]): boolean {
+  if (source === needle) {
+    path.push(source)
+    return true;
+  }
+
+  if (seen[source]) return false;
+
+  seen[source] = true;
+
+  // pre recurse
+  path.push(source)
+
+  let neighbours = graph[source]
+  for (let i = 0; i < neighbours.length; i++) {
+    if (walk(graph, neighbours[i].to, needle, seen, path)) {
+      return true;
+    }
+  }
+  // post recurse
+  path.pop()
+  return false;
 }
